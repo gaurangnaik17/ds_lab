823afdf05c9b68a942f645a6b4bbf59cf99d5f4b
Gaurang Naik <gaurangnaik61@gmail.com>
2026-08-03 22:28:50 +0530

    Initial commit

---
+++ POINTER/dsprac2b.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+// Call by Value
+void value(int a, int b)
+{
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+
+    cout << "\nAfter Call by Value:" << endl;
+    cout << "a = " << a << " b = " << b << endl;
+}
+
+// Call by Reference
+void reference(int &a, int &b)
+{
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+
+    cout << "\nAfter Call by Reference:" << endl;
+    cout << "a = " << a << " b = " << b << endl;
+}
+
+int main()
+{
+    int x = 10, y = 20;
+
+    cout << "Before Function Call:" << endl;
+    cout << "x = " << x << " y = " << y << endl;
+
+    value(x, y);
+
+    cout << "\nAfter Call by Value in Main:" << endl;
+    cout << "x = " << x << " y = " << y << endl;
+
+    reference(x, y);
+
+    cout << "\nAfter Call by Reference in Main:" << endl;
+    cout << "x = " << x << " y = " << y << endl;
+
+    return 0;
+}
