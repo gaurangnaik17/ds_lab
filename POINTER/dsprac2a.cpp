d1487216c0c4ccf741195770c99edeb7fd0bb42e
Gaurang Naik <gaurangnaik61@gmail.com>
2026-08-03 22:11:17 +0530

    Initial commit

---
+++ POINTER/dsprac2a.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[100], n;
+    int *ptr;
+
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    cout << "Enter the array elements:\n";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    // Pointer points to the last element
+    ptr = arr + n - 1;
+
+    cout << "Array elements in reverse order are: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << *ptr << " ";
+        ptr--;
+    }
+
+    return 0;
+}
